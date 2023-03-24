0dd2ff0e0966188aa2acdfc2f22fbacaf79877ca
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-24 17:46:24 +0100

    js code
    starting the js code

---
+++ src/javascript/index.js
@@ -0,0 +1,15 @@
+/*
+OBJECTIVE - when we click on the button we have to show the corresponding background image
+
+     - step 1 - find a way to get the HTML element of the buttons
+
+- step 2 - find a way to identify the user's click on the button
+
+- step 3 - uncheck the previous selected button
+
+- step 4 - mark the clicked button as if it were selected
+
+- step 5 - hide previous background image
+
+- step 6 - make the background image corresponding to the clicked button appear
+*/
